048f11f89843436b0cb6e2065f375178d94e6314
KIBASSA MALIBA JUNIOR <juniorkibassa2007@gmail.com>
2019-10-27 22:09:07 +0100

    add others tools

--- src/scrapping/key_words_provider.py
+++ src/scrapping/key_words_provider.py
@@ -23,7 +23,21 @@ class KeyWordsProvider:
     def get_tools(self):
         return ['tableau', 'powerbi','symfony', 'jquery','angular','react','react native','node js','git','github',
             'visual studio','django','flask','api rest','laravel', 'hololens', 'docker', 'jira', 'scrum', 'kanban', 
-               'azure','aws', 'teamcity', 'jenkins']
+               'azure','aws', 'teamcity', 'jenkins',"oracle", "mysql","Microsoft SQL Server", "PostgreSQL","DB2","Microsoft Access",
+                "SQLite","Teradata", "SAP Adaptative Server", "Hive", "FileMaker"
+             ,"SAP HANA", "MariaDB", "Informix", "Firebird", "microsoft azure sql database", "Vertica", "Netezza", "Ingres", "Greenplum", "mongodb","Liquibase",
+                "Buddy", "Semaphore", "GoCD", "Drone.io", "TeamCity", "Wercker", "Codeship", "Travis CI", "CircleCI", "Bamboo","Jenkins",
+                "VersionOne", "Phabricator", "Asana", "Pivotal Tracker", "Basecamp", "Visual Studio", "Trello", "Jira Software", "Mingle",
+                "Kallithea", "Beanstalk", "GitBucket", "Mercurial", "Gogs", "GitLab", "Bitbucket", "GitHub", "Subversion", "Git",
+                "SoapUI", "Katalon Studio", "ThreatModeler", "Checkmarx", "RSpec", "SpecFlow", "Pa11y", "Browsersync",
+                "Serverspec", "pytest", "BlazeMeter", "Load Impact", "Galen Framework",
+                "TestNG", "QUnit", "NUnit", "FitNesse", "Karma", "Gatling", "OWASP ZAP", "Gauntlt", "Mocha", "Jmeter",
+                "Cucumber", "JUnit", "Jasmine", "Selenium","Nagios","Keen IO","Opsgenie","Beats","Moogsoft","PagerDuty","Rollbar","Raygun",
+                "Graphite","Grafana","APImetrics","Riemann","Atlas","Runscope","Dynatrace","Sensu","Pinpoint","Prometheus"
+                ,"Vizceral","Sentry","Google Analytics","Grok","Zipkin","Zabbix","Datadog","Kibana","Elasticsearch","Logstash","Airbrake","New Relic","App Dynamics","Vector","Splunk",
+                "Morpheus", "Dokku", "Engine Yard", "OpenShift", "Cloud Foundry", "Flynn", "Azure", "OpenStack",
+                "Rackspace", "Google Cloud Platform", "Heroku", "Amazon Web Services"
+                ]
     
     def get_others(self):
         return ['cisco','intel','android','ios','unity', 'unreal engine', 'ux designer','realite virtuelle','realite augmentee','game','ihm','vr','ar','3d','electronic','iot']
