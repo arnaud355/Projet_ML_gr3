14a4d19fb1ea3ee1cc6bf817db69ba5d22f39427
KIBASSA MALIBA JUNIOR <juniorkibassa2007@gmail.com>
2019-10-27 16:39:29 +0100

    add lang_de_prog page js and html

--- src/flask_ml/app.py
+++ src/flask_ml/app.py
@@ -17,7 +17,12 @@ from pandas.io.json import json_normalize
 #nba = json_normalize(datapoints)
 from flask import Flask,render_template,Response
 from flask_restful import Resource, Api, reqparse
+import sys
+sys.path.append('../scrapping')
 
+
+langage_de_progs = ["JavaScript","Java","Python","PHP","C#","C++","CSS","Ruby","C","Objective-C","Swift","TypeScript","Scala","Shell","Go","R","PowerShell","Perl","Haskell","Kotlin", "Dart",
+"Haskell", "Ocaml","Lua", "F#","Scala","D","Swift","Erlang", "Julia"]
 
 app = Flask(__name__)
 api = Api(app)  # type: Api
@@ -27,6 +32,7 @@ parser = reqparse.RequestParser()
 parser.add_argument('x', type=int, default=False, required=False)
 parser.add_argument('y', type=int, default=False, required=False)
 
+df = pd.read_csv("../../data/indeed.predicted.csv")
 class Multiply(Resource):
     def get(self, x):
         result = x * x
@@ -43,7 +49,6 @@ def paris_vs_ville(tag):
 
 @app.route('/Paris_Vs_Ville/data')
 def paris_vs_ville_data():
-    df = pd.read_csv("../../data/indeed.predicted.csv")
     locations = np.unique(df[pd.notnull(df["localisation"])]["localisation"]).tolist()
     result = {}
     for item in locations:
@@ -52,6 +57,37 @@ def paris_vs_ville_data():
                         "moyen": statistics.mean(temp["salaire_moyen"]),
                         "max": statistics.mean(temp["salaire_max"])
                         }
+    return json.dumps(result)
+
+
+#langage_de_prog.html
+@app.route('/salaire_langage_de_prog/page/<string:tag>')
+def salaire_langage_de_prog(tag):
+    return render_template("langage_de_prog.html")
+
+@app.route('/salaire_langage_de_prog/data')
+def salaire_langage_de_prog_data():
+    locations = np.unique(df[pd.notnull(df["localisation"])]["localisation"]).tolist()
+    result = {}
+    for lg in langage_de_progs:
+        lg = lg.lower()
+        if lg not in df.columns:
+            continue
+        for index, item in enumerate(df[lg]):
+            if item == 1:
+                key = lg + "_toutes"
+                if key not in result:
+                    result[key] = df['salaire_moyen'][index]
+                else:
+                    result[key] = (result[key] + df['salaire_moyen'][index]) / 2
+                for loc in locations:
+                    if df["localisation"][index] == loc:
+                        key_ = lg + "_" + loc
+                        if key_ not in result:
+                            result[key_] = df['salaire_moyen'][index]
+                        else:
+                            result[key_] = (result[key_] + df['salaire_moyen'][index]) / 2
+
     return json.dumps(result)
 
 
